aefb160119ab64a6d0be3aa9667a76089d15f1cd
mate03vinti <mate03vinti@gmail.com>
2024-09-18 20:36:30 +0000

    primer commit SideMenu

---
+++ src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+  originalTheme: string = ' rgb(250, 158, 0)'; // Set your original theme color here
+  toggleTheme(newTheme: string) {
+    if (this.theme === newTheme) {
+      this.theme = this.originalTheme;
+    } else {
+      this.theme = newTheme;
+    }
+  }
+  animateItem(event: any) {
+    event.target.style.backgroundColor = 'red';
+  }
+  resetAnimation(event: any) {
+    event.target.style.backgroundColor = 'white'; 
+  }
+userName: string=" Mateo Vintmilla ";
+theme: string="";
+widthMenu:number=25;
+visibleMenu:boolean=true;
+
+alternateVisibility():void{
+  this.visibleMenu=!this.visibleMenu;//alterna la visibildad del menu }
+}
+setTheme(color:string):void{//Cambia l color del menu
+  this.theme=color;
+
+}
+setSize():void{
+  this.widthMenu+=10;
+  if(this.widthMenu>100){
+    this.widthMenu=25;
+  }
+}
+}
